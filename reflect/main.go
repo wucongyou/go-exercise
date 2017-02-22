66f3b481a0e6a86c17de3d5b2da37145111a3259
echo <echo940811@163.com>
2017-02-22 19:00:36 +0800

    add interface

--- reflect/main.go
+++ reflect/main.go
@@ -5,46 +5,56 @@ import (
 	"reflect"
 )
 
+type SayHello interface {
+	SayHello() string
+}
+
 type Person struct {
 }
 
 func (f *Person) SayHello() string {
+	fmt.Println("hello")
 	return "hello"
 }
 
-func when(m reflect.Method) {
-	return
+type Proxy struct {
+	Person
 }
 
-func thenReturn(v interface{}) {
-
+func (f *Proxy) SayHello() string {
+	fmt.Println("hello from proxy")
+	return "hello from proxy"
 }
 
-func mock(a interface{}) (res interface{}) {
-	for m := 0; m < reflect.TypeOf(a).NumMethod(); m++ {
-		method := reflect.TypeOf(a).Method(m)
+func methods(a interface{}) {
+	for i := 0; i < reflect.TypeOf(a).NumMethod(); i++ {
+		method := reflect.TypeOf(a).Method(i)
 		fmt.Println(method.Type)         // func(*main.MyStruct) string
 		fmt.Println(method.Name)         // GetName
 		fmt.Println(method.Type.NumIn()) // 参数个数
 		fmt.Println(method.Type.In(0))   // 参数类型
 	}
-	return
 }
 
 func main() {
 	var (
 		t reflect.Type
 		v reflect.Value
+		p = new(SayHello)
 	)
 	s := "hello"
 	t = reflect.TypeOf(s)
 	fmt.Printf("t: %v\n", t)
 	v = reflect.ValueOf(s)
 	fmt.Printf("v: %v\n", v)
-	p := new(Person)
+	*p = new(Person)
 	t = reflect.TypeOf(p)
 	fmt.Printf("t: %v\n", t)
 	v = reflect.ValueOf(p)
 	fmt.Printf("v: %v\n", v)
-	mock(new(Person))
+	*p = new(Person)
+	(*p).SayHello()
+	methods(p)
+	*p = new(Proxy)
+	(*p).SayHello()
 }
