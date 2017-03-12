fff38ba0b0aa61d131a4be277e82d8f6e8a4bee9
echo <echo940811@163.com>
2017-03-12 18:38:54 +0800

    add block comment pattern

--- main.go
+++ main.go
@@ -5,5 +5,7 @@ import "fmt"
 func main() {
 	// k, j for up, down
 	// h, l for left, right
+	//	// :10,11s#^#//#g
+	//	// :10,11s#^//##g
 	fmt.Println("In i mode, press ^x, ^o for code completion")
 }
