9c730af6509e25790c74d4c1d59db0159d7c808e
echo <echo940811@163.com>
2017-03-27 15:07:32 +0800

    add waitgroup usage

---
+++ concurrent/waitgroup.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func(id int) {
+			defer wg.Done()
+			time.Sleep(time.Second)
+			fmt.Printf("goroutine %d done\n", id)
+		}(i)
+	}
+	fmt.Println("main waiting")
+	wg.Wait()
+	fmt.Println("main done")
+}
