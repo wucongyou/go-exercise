063eb74bf571743978d8cf77792ab14a006b2286
congyou.wu <echo940811@163.com>
2017-07-27 17:14:36 +0800

    add target dir

--- markdown/md.go
+++ markdown/md.go
@@ -2,31 +2,26 @@ package markdown
 
 import (
 	"flag"
-	"fmt"
-
-	"github.com/microcosm-cc/bluemonday"
-	"github.com/russross/blackfriday"
 	"io/ioutil"
 	"os"
 	"strings"
+
+	"github.com/microcosm-cc/bluemonday"
+	"github.com/russross/blackfriday"
 )
 
 var (
-	infile  string
-	outfile string
+	infile    string
+	targetDir string
 )
 
 func init() {
 	flag.StringVar(&infile, "f", "hello.md", "input markdown filepath")
-	flag.StringVar(&outfile, "o", "", "out html filepath")
+	flag.StringVar(&targetDir, "t", "", "target dir for output html file")
 }
 
 func Md() {
 	flag.Parse()
-	if outfile == "" {
-		arr := strings.Split(infile, ".md")
-		outfile = arr[0] + ".html"
-	}
 	f, err := os.Open(infile)
 	if err != nil {
 		panic(err)
@@ -39,8 +34,15 @@ func Md() {
 	input := string(fd)
 	unsafe := blackfriday.MarkdownCommon([]byte(input))
 	html := bluemonday.UGCPolicy().SanitizeBytes(unsafe)
-	fmt.Printf("html: %s", string(html))
-	err = ioutil.WriteFile(outfile, html, 0644)
+	arr := strings.Split(infile, ".md")
+	out := arr[0] + ".html"
+	if targetDir != "" {
+		if !strings.HasSuffix(targetDir, "/") {
+			targetDir = targetDir + "/"
+		}
+		out = targetDir + out
+	}
+	err = ioutil.WriteFile(out, html, 0644)
 	if err != nil {
 		panic(err)
 	}
