8b37c39a8f74e3a837f5eb9df0bda1999f4cca58
congyou.wu <echo940811@163.com>
2017-10-21 01:11:06 +0800

    add get, set, del impl to memcache cli

--- memcache/cmd/main.go
+++ memcache/cmd/main.go
@@ -1,36 +1,189 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
+	"strconv"
+	"strings"
 
 	"go-exercise/memcache/dao"
 )
 
+var (
+	ErrSyntaxErr = errors.New("ERR syntax error")
+)
+
+// get key
+// keys *
 func main() {
 	var (
-		host  string
-		port  int64
-		vtype string
+		host string
+		port int64
 	)
 	flag.StringVar(&host, "h", "127.0.0.1", "host")
 	flag.Int64Var(&port, "p", 11211, "port")
-	flag.StringVar(&vtype, "t", "string", "value type")
 	flag.Parse()
-	fmt.Printf("connecting to %s:%d\n", host, port)
 	d := dao.New(&dao.Conf{
 		Host: host,
 		Port: port,
 	})
-	d.Set("ping", []byte("pong"), 1800)
-	res, err := d.Get("ping")
-	if err != nil {
-		panic(err)
+	fmt.Printf("connected to %s:%d\n", host, port)
+	for {
+		fmt.Printf("%s:%d> ", host, port)
+		cmdLine := ""
+		scanLine(&cmdLine)
+		args := strings.Split(cmdLine, " ")
+		if len(args) == 0 {
+			continue
+		}
+		if args[0] == "exit" {
+			return
+		}
+		recv(d, args)
 	}
-	fmt.Printf("res: %s\n", res)
-	strRes, err := d.FormatWithType("ping", "string")
-	if err != nil {
-		panic(err)
+}
+
+func scanLine(s *string) {
+	var c byte
+	var err error
+	var b []byte
+	for err == nil {
+		_, err = fmt.Scanf("%c", &c)
+		if c != '\n' {
+			b = append(b, c)
+		} else {
+			break
+		}
 	}
-	fmt.Printf("res: %s", strRes)
+	*s = string(b)
+}
+
+func recv(d *dao.Dao, args []string) {
+	if len(args) == 0 {
+		fmt.Println("Err nil command")
+		return
+	}
+	cmd := parseCMD(args[0])
+	if cmd == nil {
+		fmt.Printf("ERR unknown command '%s'\n", args[0])
+		return
+	}
+	paramNum := len(args) - 1
+	if paramNum < cmd.MinParamNum || paramNum > cmd.MaxParamNum {
+		fmt.Printf("ERR wrong number of arguments for '%s' command\nUsage: %s\n", cmd.Name, cmd.Usage)
+		return
+	}
+	var err error
+	switch cmd {
+	// set key value [expiration] [flags]
+	case _cmdSet:
+		key := args[1]
+		value := args[2]
+		exp := int64(0)
+		flags := uint64(0)
+		if paramNum >= 3 {
+			exp, err = strconv.ParseInt(args[3], 10, 32)
+			if err != nil {
+				err = ErrSyntaxErr
+				fmt.Printf("%v, expiration should be int\nUsage: %s\n", err, cmd.Usage)
+				return
+			}
+		}
+		if paramNum >= 4 {
+			flags, err = strconv.ParseUint(args[4], 10, 32)
+			if err != nil {
+				err = ErrSyntaxErr
+				fmt.Printf("%v, flags should be int\nUsage: %s\n", err, cmd.Usage)
+				return
+			}
+		}
+		if err = d.Set(key, []byte(value), int32(exp), uint32(flags)); err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println("OK")
+		return
+		// del key
+	case _cmdDel:
+		key := args[1]
+		if err = d.Delete(key); err != nil {
+			resp := new(dao.Resp)
+			resp.FromErr(err)
+			fmt.Println(resp.Format())
+			return
+		}
+		fmt.Println("OK")
+		return
+		// get key [binary|string]
+	case _cmdGet:
+		key := args[1]
+		vtp := ""
+		if paramNum >= 2 {
+			vtp = args[2]
+		}
+		res := d.GetWithType(key, vtp)
+		fmt.Println(res.Format())
+		return
+	case _cmdKeys:
+		fmt.Println("keys command not impled yet")
+		return
+	}
+}
+
+type Cmd struct {
+	Name        string
+	MinParamNum int
+	MaxParamNum int
+	Params      []string
+	Usage       string
+}
+
+const (
+	_actionDel  = "del"
+	_actionSet  = "set"
+	_actionGet  = "get"
+	_actionKeys = "keys"
+)
+
+var (
+	_cmdDel = &Cmd{
+		Name:        _actionDel,
+		MinParamNum: 1,
+		MaxParamNum: 1,
+		Usage:       "del key",
+	}
+	_cmdSet = &Cmd{
+		Name:        _actionSet,
+		MinParamNum: 2,
+		MaxParamNum: 4,
+		Usage:       "set key value [expire seconds or absolute ts, default 0] [flag, default 0]",
+	}
+	_cmdGet = &Cmd{
+		Name:        _actionGet,
+		MinParamNum: 1,
+		MaxParamNum: 2,
+		Usage:       "get key [binary|string, default string]",
+	}
+	_cmdKeys = &Cmd{
+		Name:        _actionKeys,
+		MinParamNum: 1,
+		MaxParamNum: 1,
+		Usage:       "keys pattern",
+	}
+)
+
+func parseCMD(key string) *Cmd {
+	switch key {
+	case _actionDel:
+		return _cmdDel
+	case _actionSet:
+		return _cmdSet
+	case _actionGet:
+		return _cmdGet
+	case _actionKeys:
+		return _cmdKeys
+	default:
+		return nil
+	}
 }
--- memcache/dao/dao.go
+++ memcache/dao/dao.go
@@ -6,10 +6,11 @@ import (
 	"github.com/bradfitz/gomemcache/memcache"
 )
 
-const (
-	_string = "string"
-	_byte   = "byte"
-)
+func New(c *Conf) *Dao {
+	return &Dao{
+		mc: memcache.New(fmt.Sprintf("%s:%d", c.Host, c.Port)),
+	}
+}
 
 type Dao struct {
 	mc *memcache.Client
@@ -20,38 +21,82 @@ type Conf struct {
 	Port int64
 }
 
-func New(c *Conf) *Dao {
-	return &Dao{
-		mc: memcache.New(fmt.Sprintf("%s:%d", c.Host, c.Port)),
+const (
+	RespTypeOk    = "ok"
+	RespTypeError = "error"
+	VTypeString   = "string"
+	VTypeBinary   = "binary"
+)
+
+type Resp struct {
+	Type    string
+	Content string
+}
+
+func (r *Resp) Format() string {
+	if r.Type == RespTypeOk {
+		return fmt.Sprintf("%s", r.Content)
+	} else {
+		return fmt.Sprintf("%s, %s", r.Type, r.Content)
 	}
 }
 
-func (d *Dao) FormatWithType(key, t string) (res string, err error) {
-	data, err := d.Get(key)
+func (r *Resp) FromItem(item *memcache.Item, t string) {
+	res := ""
+	switch parseVType(t) {
+	case VTypeBinary:
+		res = fmt.Sprintf("%v", item.Value)
+	case VTypeString:
+		res = string(item.Value)
+	}
+	r.Type = RespTypeOk
+	r.Content = fmt.Sprintf("%s [flags: %d]", res, item.Flags)
+}
+
+func parseVType(t string) string {
 	switch t {
-	case _string:
-		res = string(data)
-	case _byte:
-		res = fmt.Sprintf("%v", data)
+	case "binary", "bin":
+		return VTypeBinary
+	default:
+		return VTypeString
 	}
-	return
 }
 
-func (d *Dao) Get(key string) (res []byte, err error) {
-	item, err := d.mc.Get(key)
-	if err != nil {
+func (r *Resp) FromErr(err error) {
+	if err == memcache.ErrCacheMiss {
+		r.Type = RespTypeOk
+		r.Content = "(nil)"
 		return
 	}
-	res = item.Value
+	r.Type = RespTypeError
+	r.Content = fmt.Sprintf("%v", err)
 	return
 }
 
-func (d *Dao) Set(key string, value []byte, exp int32) (err error) {
-	d.mc.Set(&memcache.Item{
+func (d *Dao) GetWithType(key, t string) (resp *Resp) {
+	resp = new(Resp)
+	item, err := d.Get(key)
+	if err != nil {
+		resp.FromErr(err)
+		return
+	}
+	resp.FromItem(item, t)
+	return
+}
+
+func (d *Dao) Get(key string) (item *memcache.Item, err error) {
+	item, err = d.mc.Get(key)
+	return
+}
+
+func (d *Dao) Set(key string, value []byte, exp int32, flags uint32) (err error) {
+	item := &memcache.Item{
 		Key:        key,
 		Value:      value,
 		Expiration: exp,
-	})
+		Flags:      flags,
+	}
+	err = d.mc.Set(item)
 	return
 }
 
